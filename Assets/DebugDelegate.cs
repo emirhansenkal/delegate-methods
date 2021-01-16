614b13e410de1ae309e25e28e4ae11c8e577832c
Emirhan Şenkal <emirhansenkal1@gmail.com>
2021-01-16 23:38:30 +0300

    Initial Project

---
+++ Assets/DebugDelegate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugDelegate : MonoBehaviour
+{
+
+    public delegate void DelegateDebug();//Tanımlama
+
+    public DelegateDebug delegateDebug;//Obje oluşturma
+    
+    void Start()
+    {
+
+        delegateDebug += Debug1;
+        AddMethod(Debug2);
+
+        RemoveMethod(Debug1);
+        
+        if(delegateDebug !=null)
+            delegateDebug();
+    }
+
+    private void Debug1()
+    {
+        Debug.Log("Debug 1");
+    }
+
+    private void Debug2()
+    {
+        Debug.Log("Debug 2");
+    }
+
+    public void AddMethod(DelegateDebug method)
+    {
+        delegateDebug += method;
+    }
+    
+    public void RemoveMethod(DelegateDebug method)
+    {
+        delegateDebug -= method;
+    }
+}
---
+++ Assets/MathTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathTest : MonoBehaviour
+{
+    public delegate int CalculateDelegate(int x1, int x2);
+
+    public CalculateDelegate calculateDelegate;
+
+    private void Start()
+    {
+        calculateDelegate += Addition;
+        calculateDelegate += Subtraction;
+        calculateDelegate += Multiplication;
+        calculateDelegate += (int a, int b) => // Lambda Expression
+        {
+            Debug.Log("Bölündü");
+            return a / b;
+        };
+
+        //calculateDelegate(13, 3);
+
+        Delegate[] funcs = calculateDelegate.GetInvocationList();
+
+        for (int i = 0; i < funcs.Length; i++)
+        {
+            int result = ((CalculateDelegate)funcs[i]).Invoke(13,3);
+            Debug.Log(result);
+        }
+    }
+
+    private int Addition(int a, int b)
+    {
+        Debug.Log("Toplandı");
+        return a + b;
+    }
+
+    private int Subtraction(int a, int b)
+    {
+        Debug.Log("Çıkarıldı");
+        return a - b;
+    }
+
+    private int Multiplication(int a, int b)
+    {
+        Debug.Log("Çarpıldı");
+        return a * b;
+    }
+}
